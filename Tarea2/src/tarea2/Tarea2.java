b3bdb9fda8e9bc3d0ddb9e9ab84073b31e091b7f
erandilozano5im9 <42345771+erandilozano5im9@users.noreply.github.com>
2018-08-20 21:40:37 -0500

    Add files via upload

---
+++ Tarea2/src/tarea2/Tarea2.java
@@ -0,0 +1,10 @@
+
+package tarea2;
+
+public class Tarea2 {
+    //Erandi Lozano Herrera 5IM9
+    public static void main(String[] args) {
+        System.out.println("Hola mundo unu");
+    }
+    
+}
